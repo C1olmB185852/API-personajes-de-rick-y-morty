128b32e1ba2c6b49d96c2db298d8479c758c1e60
C1olmB185852 <dan.ielestebanmat@gmail.com>
2024-02-17 20:37:05 -0500

    Actualizacion Imagen

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -6,8 +6,14 @@ const Characters = ({characters = []}) => {
             {
             characters.map((item, index) => (
                 <div key={index} className='col'>
-                    <div className='card'>
+                    <div className='card' style={{minWidth: "200px"}}>
                         <img src={item.image} alt=''/>
+                        <div className='card-boby'>
+                            <h5 className='card-title'>{item.name}</h5>
+                            <hr />
+                            <p>Species: {item.species}</p>
+                            <p>location: {item.location.name}</p>
+                        </div>
                     </div>
                 </div>
                 ))}
